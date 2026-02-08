70d51b0f626bb95883b1b5180e2a5dffe8c7bad2
Luu Thao <luuthao23@gmail.com>
2026-02-08 13:31:55 +0700

    Add FIS-enhanced DeepJSCC while preserving baseline model

--- model.py
+++ model.py
@@ -1,118 +1,230 @@
+# -*- coding: utf-8 -*-
 """
-Modified model.py to add FIS-enhanced version
+Created on Tue Dec  11:00:00 2023
+
+@author: chun
 """
 
 import torch
 import torch.nn as nn
-from fis_modules import FIS_ImportanceAssessment, FIS_BitAllocation, AdaptiveQuantizer
+from channel import Channel
 
 
-class JSCC(nn.Module):
-    """Original JSCC model (unchanged for baseline comparison)"""
+""" def _image_normalization(norm_type):
+    def _inner(tensor: torch.Tensor):
+        if norm_type == 'nomalization':
+            return tensor / 255.0
+        elif norm_type == 'denormalization':
+            return (tensor * 255.0).type(torch.FloatTensor)
+        else:
+            raise Exception('Unknown type of normalization')
+    return _inner """
 
-    def __init__(self, C=16, channel_num=16):
-        super(JSCC, self).__init__()
 
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, channel_num, kernel_size=5, stride=1, padding=2),
-        )
+def ratio2filtersize(x: torch.Tensor, ratio):
+    if x.dim() == 4:
+        # before_size = np.prod(x.size()[1:])
+        before_size = torch.prod(torch.tensor(x.size()[1:]))
+    elif x.dim() == 3:
+        # before_size = np.prod(x.size())
+        before_size = torch.prod(torch.tensor(x.size()))
+    else:
+        raise Exception('Unknown size of input')
+    encoder_temp = _Encoder(is_temp=True)
+    z_temp = encoder_temp(x)
+    # c = before_size * ratio / np.prod(z_temp.size()[-2:])
+    c = before_size * ratio / torch.prod(torch.tensor(z_temp.size()[-2:]))
+    return int(c)
 
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(channel_num, C, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, C, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, C, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, C, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, 3, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.Sigmoid()
-        )
+
+class _ConvWithPReLU(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+        super(_ConvWithPReLU, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.prelu = nn.PReLU()
+
+        nn.init.kaiming_normal_(self.conv.weight, mode='fan_out', nonlinearity='leaky_relu')
 
     def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return encoded, decoded
+        x = self.conv(x)
+        x = self.prelu(x)
+        return x
 
 
-class JSCC_FIS(nn.Module):
-    """FIS-Enhanced JSCC model"""
+class _TransConvWithPReLU(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, activate=nn.PReLU(), padding=0, output_padding=0):
+        super(_TransConvWithPReLU, self).__init__()
+        self.transconv = nn.ConvTranspose2d(
+            in_channels, out_channels, kernel_size, stride, padding, output_padding)
+        self.activate = activate
+        if activate == nn.PReLU():
+            nn.init.kaiming_normal_(self.transconv.weight, mode='fan_out',
+                                    nonlinearity='leaky_relu')
+        else:
+            nn.init.xavier_normal_(self.transconv.weight)
 
-    def __init__(self, C=16, channel_num=16):
-        super(JSCC_FIS, self).__init__()
+    def forward(self, x):
+        x = self.transconv(x)
+        x = self.activate(x)
+        return x
 
-        # Same encoder/decoder as baseline
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, C, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(C, channel_num, kernel_size=5, stride=1, padding=2),
-        )
 
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(channel_num, C, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, C, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, C, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, C, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(C, 3, kernel_size=5, stride=2, padding=2, output_padding=1),
-            nn.Sigmoid()
-        )
+class _Encoder(nn.Module):
+    def __init__(self, c=1, is_temp=False, P=1):
+        super(_Encoder, self).__init__()
+        self.is_temp = is_temp
+        # self.imgae_normalization = _image_normalization(norm_type='nomalization')
+        self.conv1 = _ConvWithPReLU(in_channels=3, out_channels=16, kernel_size=5, stride=2, padding=2)
+        self.conv2 = _ConvWithPReLU(in_channels=16, out_channels=32, kernel_size=5, stride=2, padding=2)
+        self.conv3 = _ConvWithPReLU(in_channels=32, out_channels=32,
+                                    kernel_size=5, padding=2)  # padding size could be changed here
+        self.conv4 = _ConvWithPReLU(in_channels=32, out_channels=32, kernel_size=5, padding=2)
+        self.conv5 = _ConvWithPReLU(in_channels=32, out_channels=2*c, kernel_size=5, padding=2)
+        self.norm = self._normlizationLayer(P=P)
 
-        # FIS modules (NEW)
+    @staticmethod
+    def _normlizationLayer(P=1):
+        def _inner(z_hat: torch.Tensor):
+            if z_hat.dim() == 4:
+                batch_size = z_hat.size()[0]
+                # k = np.prod(z_hat.size()[1:])
+                k = torch.prod(torch.tensor(z_hat.size()[1:]))
+            elif z_hat.dim() == 3:
+                batch_size = 1
+                # k = np.prod(z_hat.size())
+                k = torch.prod(torch.tensor(z_hat.size()))
+            else:
+                raise Exception('Unknown size of input')
+            # k = torch.tensor(k)
+            z_temp = z_hat.reshape(batch_size, 1, 1, -1)
+            z_trans = z_hat.reshape(batch_size, 1, -1, 1)
+            tensor = torch.sqrt(P * k) * z_hat / torch.sqrt((z_temp @ z_trans))
+            if batch_size == 1:
+                return tensor.squeeze(0)
+            return tensor
+        return _inner
+
+    def forward(self, x):
+        # x = self.imgae_normalization(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        if not self.is_temp:
+            x = self.conv5(x)
+            x = self.norm(x)
+        return x
+
+
+class _Decoder(nn.Module):
+    def __init__(self, c=1):
+        super(_Decoder, self).__init__()
+        # self.imgae_normalization = _image_normalization(norm_type='denormalization')
+        self.tconv1 = _TransConvWithPReLU(
+            in_channels=2*c, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.tconv2 = _TransConvWithPReLU(
+            in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.tconv3 = _TransConvWithPReLU(
+            in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.tconv4 = _TransConvWithPReLU(in_channels=32, out_channels=16, kernel_size=5, stride=2, padding=2, output_padding=1)
+        self.tconv5 = _TransConvWithPReLU(
+            in_channels=16, out_channels=3, kernel_size=5, stride=2, padding=2, output_padding=1,activate=nn.Sigmoid())
+        # may be some problems in tconv4 and tconv5, the kernal_size is not the same as the paper which is 5
+
+    def forward(self, x):
+        x = self.tconv1(x)
+        x = self.tconv2(x)
+        x = self.tconv3(x)
+        x = self.tconv4(x)
+        x = self.tconv5(x)
+        # x = self.imgae_normalization(x)
+        return x
+
+
+class DeepJSCC(nn.Module):
+    def __init__(self, c, channel_type='AWGN', snr=None):
+        super(DeepJSCC, self).__init__()
+        self.encoder = _Encoder(c=c)
+        if snr is not None:
+            self.channel = Channel(channel_type, snr)
+        self.decoder = _Decoder(c=c)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        if hasattr(self, 'channel') and self.channel is not None:
+            z = self.channel(z)
+        x_hat = self.decoder(z)
+        return x_hat
+
+    def change_channel(self, channel_type='AWGN', snr=None):
+        if snr is None:
+            self.channel = None
+        else:
+            self.channel = Channel(channel_type, snr)
+
+    def get_channel(self):
+        if hasattr(self, 'channel') and self.channel is not None:
+            return self.channel.get_channel()
+        return None
+
+    def loss(self, prd, gt):
+        criterion = nn.MSELoss(reduction='mean')
+        loss = criterion(prd, gt)
+        return loss
+# =====================================================
+# FIS-ENHANCED MODEL (KẾ THỪA – KHÔNG PHÁ BASELINE)
+# =====================================================
+# =====================================================
+# FIS modules
+# =====================================================
+from fis_modules import (
+    FIS_ImportanceAssessment,
+    FIS_BitAllocation,
+    AdaptiveQuantizer
+)
+class DeepJSCC_FIS(DeepJSCC):
+    """
+    Encoder / Decoder / Channel giống 100% DeepJSCC
+    Chỉ thêm FIS ở latent space
+    """
+    def __init__(self, c, channel_type='AWGN', snr=None):
+        super().__init__(c, channel_type, snr)
         self.fis_importance = FIS_ImportanceAssessment()
         self.fis_allocation = FIS_BitAllocation()
         self.quantizer = AdaptiveQuantizer()
 
-    def forward(self, x, snr=10.0, target_rate=0.5, return_info=False):
-        """
-        Forward with FIS-based adaptive encoding
+    def forward(self, x, fis_snr=10.0, target_rate=0.5, return_info=False):
+        z = self.encoder(x)
 
-        Args:
-            x: (B, 3, H, W) input images
-            snr: channel SNR in dB
-            target_rate: rate budget [0, 1]
-            return_info: return intermediate info
+        importance = self.fis_importance(z)
+        bits = self.fis_allocation(
+            importance,
+            snr=fis_snr,
+            target_rate=target_rate
+        )
+        z_q = self.quantizer(z, bits)
 
-        Returns:
-            encoded: (B, channel_num, H', W') encoded features
-            decoded: (B, 3, H, W) decoded images
-            info: dict (if return_info=True)
-        """
-        # Encode
-        encoded = self.encoder(x)  # (B, channel_num, H', W')
+        if self.channel is not None:
+            z_q = self.channel(z_q)
 
-        # FIS processing
-        importance_map = self.fis_importance(encoded)  # (B, H', W')
-        bit_allocation = self.fis_allocation(importance_map, snr, target_rate)  # (B, H', W')
-        encoded_quantized = self.quantizer(encoded, bit_allocation)  # (B, channel_num, H', W')
-
-        # Decode
-        decoded = self.decoder(encoded_quantized)  # (B, 3, H, W)
+        x_hat = self.decoder(z_q)
 
         if return_info:
-            info = {
-                'importance_map': importance_map,
-                'bit_allocation': bit_allocation,
-                'avg_bits': bit_allocation.float().mean().item()
+            return x_hat, {
+                "importance_map": importance.detach(),
+                "avg_bits": bits.float().mean().item()
             }
-            return encoded_quantized, decoded, info
-        else:
-            return encoded_quantized, decoded
+
+        return x_hat
+
+if __name__ == '__main__':
+    model = DeepJSCC(c=20)
+    print(model)
+    x = torch.rand(1, 3, 128, 128)
+    y = model(x)
+    print(y.size())
+    print(y)
+    print(model.encoder.norm)
+    print(model.encoder.norm(y))
+    print(model.encoder.norm(y).size())
+    print(model.encoder.norm(y).size()[1:])
